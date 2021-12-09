b5bdc55fdedf9f4002c4aba4c7ada2a422476985
rafabrisighello <rafael.ricciardi@yahoo.com.br>
2021-12-09 18:56:48 -0300

    Configuração de banco de dados H2

--- src/main/java/com/rafael/petshop/domain/Categoria.java
+++ src/main/java/com/rafael/petshop/domain/Categoria.java
@@ -3,6 +3,12 @@ package com.rafael.petshop.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	/**
@@ -10,7 +16,8 @@ public class Categoria implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	

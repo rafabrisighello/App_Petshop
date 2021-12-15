ff28d84620e12aa88fde5974322851ecfe3b59ef
rafabrisighello <rafael.ricciardi@yahoo.com.br>
2021-12-15 11:34:02 -0300

    CRUD de Categoria: POST, PUT, DELETE + Manipulação de Excessões

--- src/main/java/com/rafael/petshop/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/rafael/petshop/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.rafael.petshop.service.exceptions.DataIntegrityException;
 import com.rafael.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @ControllerAdvice
@@ -17,5 +18,12 @@ public class ResourceExceptionHandler {
 		
 		StandardError erro = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> objectNotFound(DataIntegrityException e, HttpServletRequest request){
+		
+		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
 	}
 }
